fc5bba2abaee6459ce284ebb05420b2c37ffbf5b
meretska-sofiia <meretska.sofiia@gmail.com>
2022-11-16 19:58:13 +0200

    hw 01

---
+++ README.md
@@ -0,0 +1,4 @@
+case "list" - https://monosnap.com/file/ttOC3QNWVUDqBcRdGQ1X58CLnNkaYu case "get" -
+https://monosnap.com/file/xW6K4pDMkhPzHXOZRwLVmgUnEz36rf case "add" -
+https://monosnap.com/file/iYqITZTPM4wvfrozXLNBayJK3YPpct case "remove" -
+https://monosnap.com/file/cpU7Jdqjq3JnOECKhpV3fc75SCWMNk
---
+++ contacts.js
@@ -0,0 +1,76 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const contactsPath = path.resolve('./db/contacts.json');
+
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    const parseData = JSON.parse(data);
+    console.table(parseData);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getContactById = async contactId => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    const parseData = JSON.parse(data);
+    const contact = parseData.find((el, indx) => {
+      if (el.id === contactId.toString()) {
+        console.log(data[indx]);
+        return data[indx];
+      }
+    });
+    console.table(contact);
+    return contact;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeContact = async contactId => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    const parseData = JSON.parse(data);
+    const index = parseData.findIndex(el => el.id === contactId);
+    parseData.splice(index, 1);
+    console.table(parseData);
+    return await fs.writeFile(contactsPath, JSON.stringify(parseData), 'utf8');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addContact = async (name, email, phone) => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    const parseData = JSON.parse(data);
+    const id = parseData.reduce((acc, el) => {
+      if (Number(el.id) > acc) {
+        acc = Number(el.id);
+        return acc;
+      }
+    }, 0);
+    const newContact = {
+      id: (id + 1).toString(),
+      name,
+      email,
+      phone,
+    };
+    const newContacts = [...parseData, newContact];
+    console.table(newContacts);
+    return await fs.writeFile(contactsPath, JSON.stringify(newContacts), 'utf8');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
